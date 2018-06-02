a37e4807aa070cb3a721420cb17ce8f093748951
Luke Melvin <luke@lucasmelvin.com>
2018-06-02 17:12:38 -0500

    renamed switch.py to pyihomeapi.py and refactored quite a few things

---
+++ pyihome/pyihomeapi.py
@@ -0,0 +1,50 @@
+import requests
+import json
+
+class ApiUrls:
+    def __init__(self):        
+        self.login = "https://www.ihomeaudio.com/api/v3/login/"
+        self.api_base = "https://api.evrythng.com/thngs"
+        self.devices = self.api_base + "?sortOrder=ASCENDING"
+
+class ApiHeaders:
+    def __init__(self):        
+        self._api_key = ""
+
+API_URLS = ApiUrls()
+API_HEADERS = ApiHeaders()
+
+class PyiHomeApi:
+    def __init__(self, email, password):
+        self._email = email
+        self._password = password        
+        self.login()
+
+    def api_call(self, url, type, formPayload, jsonPayload = None):
+        headers = {"Authorization": API_HEADERS._api_key}
+        response = requests.request(type, url, data=formPayload, json=jsonPayload, headers=headers)
+        return response
+    
+    def get_devices(self):        
+        response = self.api_call(API_URLS.devices, "GET", None)        
+        return response.json()
+
+    def login(self):
+        payload = {"password": self._password, "email": self._email}
+        response = self.api_call(API_URLS.login, "POST", payload)        
+        API_HEADERS._api_key = response.json()["everythng_api_key"]
+
+    class Switch:
+
+        def turn_on(self, device_id):
+            self._set_state(device_id, 1)
+        
+        def turn_off(self, device_id):
+            self._set_state(device_id, 0)
+        
+        def _set_state(self, device_id, state):
+            if state == None:
+                state = 0
+            url = API_URLS.api_base + "/" + device_id + "/properties/targetpowerstate1"
+            payload = [{"value":str(state)}]
+            PyiHomeApi.api_call(self, url, "PUT", None, payload)
