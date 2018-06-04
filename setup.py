38a25ec3a197905ce38d7c76419ea0bf3db2494e
Luke Melvin <luke@lucasmelvin.com>
2018-06-04 00:06:12 -0500

    bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyihome",
-    version="0.0.1",
+    version="0.0.2",
     author="lmelvin",
     author_email="luke@lucasmelvin.com",
     description="Python library to interface with iHome API.",
